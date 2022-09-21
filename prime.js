bfe6fe53fd8f0ab18925ee8a8a18a88cc62315fb
Vishal-Gupta07 <110368285+Vishal-Gupta07@users.noreply.github.com>
2022-09-21 15:05:15 +0530

    Revert "code fixed by vishal"

--- prime.js
+++ prime.js
@@ -1,25 +1,18 @@
 function CheckPrime(num){
 
-let count=0;
 for(i=0;i<=num;i++){
       if(num%i==0){
       count++
      }
-
-  if(count==2){
-    return true;
-     }
-      return false;
-  	
-	
 }
 
-
+if(count==2){
+  return true;
+  }else{
+    return false;
+  }
+}
 
 let res = checkPrime(13)
 
-if(res=true){
-console.log("Prime");
-}else{
-console.log("Not Prime")
-}
+console.log(res)
